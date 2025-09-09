dd8989d4863c111f9ee82f2c4d5bf1cb8f20a0bb
Md. Redwan Hossen <me.redwanhossen@gmail.com>
2025-09-09 14:28:00 +0600

    model

---
+++ Models/model/Department.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ThesisNest.Models.model
+{
+    public class Department
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Department Name is required")]
+        [StringLength(100)]
+        public string Name { get; set; }
+    }
+}
---
+++ Models/model/User.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace ThesisNest.Models.model
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string FullName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        public string PasswordHash { get; set; } // Store hashed password
+
+        [Required]
+        public string Role { get; set; } // e.g., Student / Supervisor / Admin
+
+        public bool IsApproved { get; set; }  // Supervisor approval
+
+        public string ContactNumber { get; set; }
+
+    }
+}
