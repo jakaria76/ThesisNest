04536b905f07d9870a8ee5fc705b7d8ee6e8dac4
MD.JAKARIA <150427217+jakaria76@users.noreply.github.com>
2025-09-11 14:56:34 +0600

    change

--- obj/Debug/net9.0/ThesisNest.AssemblyInfo.cs
+++ obj/Debug/net9.0/ThesisNest.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ThesisNest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+453af35bd0ff09e05b164b128ace6244c3c55aae")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+374bc497e800c54c1bb60bc0a1277ebf12aedefa")]
 [assembly: System.Reflection.AssemblyProductAttribute("ThesisNest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ThesisNest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
